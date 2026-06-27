a9aa2def10a2e5a36b2c79b82c079f78ec2d1330
MUDROis <mudro.is@yandex.ru>
2026-06-27 03:18:01 +0300

    feat: implement Progress module with localStorage-backed get/set/getAll/getStats

--- js/progress.js
+++ js/progress.js
@@ -1,2 +1,40 @@
-// Progress tracking module
-// Later: save/load progress from localStorage
+/* js/progress.js */
+const Progress = {
+  STORAGE_KEY: 'espanol_progress',
+
+  _load() {
+    try {
+      return JSON.parse(localStorage.getItem(this.STORAGE_KEY) || '{}');
+    } catch {
+      return {};
+    }
+  },
+
+  _save(data) {
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(data));
+  },
+
+  get(dialogId) {
+    const data = this._load();
+    return data[dialogId] || { completed: false, score: null, lastStep: 0 };
+  },
+
+  set(dialogId, updates) {
+    const data = this._load();
+    data[dialogId] = { ...this.get(dialogId), ...updates };
+    this._save(data);
+  },
+
+  getAll() {
+    return this._load();
+  },
+
+  getStats() {
+    const data = this._load();
+    const entries = Object.values(data);
+    return {
+      total: Object.keys(data).length,
+      completed: entries.filter(e => e.completed).length
+    };
+  }
+};
